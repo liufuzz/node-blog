786555e32b9a997a8d848f01137385a944929dc1
liufuzz <liufuzz@163.com>
2019-05-02 20:30:42 +0800

    blog-1

---
+++ blog-1/app.js
@@ -0,0 +1,28 @@
+const handelBlogRouter = require('./src/router/blog');
+const handleUserRouter = require('./src/router/user');
+
+const serverHandle = (req, res) => {
+  //设置返回格式JSON
+  res.setHeader('Content-type', 'application/json');
+
+  //处理blog路由
+  const blogData = handelBlogRouter(req, res);
+  if (blogData) {
+    res.end(JSON.stringify(blogData));
+    return;
+  }
+
+  //处理user路由
+  const userData = handleUserRouter(req, res);
+  if (userData) {
+    res.end(JSON.stringify(userData));
+    return;
+  }
+
+  //未命中路由， 返回404
+  res.writeHead(404, { 'Content-type': 'text/plain' });
+  res.write('404 Not Found\n');
+  res.end();
+};
+
+module.exports = serverHandle;
